8fe4be9b6941e92ba45714f79c8975001dc0d67b
melend14 <mel@Brandons-MacBook-Pro.local>
2025-10-25 17:26:56 -0400

    project init

---
+++ budget-tracker/app/layout.tsx
@@ -0,0 +1,20 @@
+
+import "./ui/globals.css";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
